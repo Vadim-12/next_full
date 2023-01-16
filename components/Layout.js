2a1052091437f45e5d5dd421946fd4eb072a9dec
Vadim-12 <66643471+Vadim-12@users.noreply.github.com>
2023-01-16 14:34:29 +0300

    3

--- components/Layout.js
+++ components/Layout.js
@@ -2,11 +2,28 @@ import Head from 'next/head'
 import Header from './Header'
 import Footer from './Footer'
 import PictureWindow from './PictureWIndow'
+import Menu from './Menu'
+import { isMobileMenuActiveAtom } from '../store/atoms'
+import { useAtom } from 'jotai'
 
 const Layout = ({children}) => {
+  const [isMobileMenuActive, setIsMobileMenuActive] = useAtom(isMobileMenuActiveAtom)
+
+  function toggleMobileMenu() {
+    setIsMobileMenuActive(prev => !prev)
+  }
+
   return (
     <>
       <PictureWindow/>
+      <button
+        id="blink"
+        onClick={toggleMobileMenu}
+        className={'menu-btn' + (isMobileMenuActive ? ' active' : '')}>
+        <span></span></button>
+      <div className={'menu-window' + (isMobileMenuActive ? ' active' : '')}>
+        <Menu />
+      </div>
       <Head>
         <meta charSet='utf-8'/>
         <title>Онлайн Галерея</title>
